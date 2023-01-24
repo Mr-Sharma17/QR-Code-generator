8fe3be4643480f219581357c29795b2c5e9e8e32
Mr-Sharma17 <as.ashishsharma980@gmail.com>
2023-01-24 20:40:31 +0530

    added js file

---
+++ index.js
@@ -0,0 +1,26 @@
+const container = document.getElementById("container");
+const userInput = document.querySelector(".form input");
+const generateBtn = document.querySelector(".form button");
+const qrImg = document.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+
+    let qrValue = userInput.value.trim();
+    if (!qrValue || preValue === qrValue) return; //checking if user entered value is empty
+
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        container.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+userInput.addEventListener("keyup", () => {
+    if (!userInput.value.trim()) {
+        container.classList.remove("active");
+        preValue = "";
+    }
+});
